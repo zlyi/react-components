c49822f17c9c163e3d3c2897c17dca3b6320a8b0
zlyi <zlyi@Ctrip.com>
2016-01-07 11:29:50 +0800

    add components

--- js/app.js
+++ js/app.js
@@ -3,7 +3,7 @@
 require('../css/style.css');
 
  
-var Toast = require('../components/toast/test');
+var Toast = require('../components/toast/index');
 
 var Alert = require('../components/alert/index');
 
@@ -11,42 +11,63 @@ var Switch = require('../components/switch/index');
 
 var Radio = require('../components/radio/index');
 
+var Modal = require('../components/modal/index');
+
 
  var React = require('react');
 
   var App = React.createClass({
   
    getInitialState:function() {
-    return { alertIsShow: null ,toastIsShow:null};
+    return { alertIsShow: null ,toastIsShow:null,modalIsShow:null};
   },
     showAlert:function(evt){
-    	      evt&&evt.preventDefault();
+    	 evt&&evt.preventDefault();
 
-    this.setState({
+    this.updateState({
       alertIsShow: true
     }); 
       },
   showToast:function(evt) {
   	      evt&&evt.preventDefault();
  
-   this.setState({
+   this.updateState({
       toastIsShow: true
     });
+  },   
+  showModal:function(evt) {
+          evt&&evt.preventDefault();
+ 
+   this.updateState({
+      modalIsShow: true
+    });
   }, 
-  render: function() {
-       console.log(JSON.stringify(this.state));
+  updateState:function(newState){ 
+
+     var defaultState={alertIsShow:null,toastIsShow:null,modalIsShow:null};
+
+     Object.assign(defaultState, newState);
+
+     this.setState(defaultState);   
+ 
+  },
+
+  render: function() { 
      return (
       <div>
              <div onClick={this.showAlert}>Alert</div>
-<div  onClick={this.showToast}>Toast</div> 
+            <div  onClick={this.showToast}>Toast</div> 
+            <div  onClick={this.showModal}>Modal</div> 
 
   <Alert isShow={this.state.alertIsShow} message="警告"  /><br/><br/>
 
              <Toast isShow={this.state.toastIsShow} message="提示"  duration="2000" /><br/><br/>
  
- 		    <Radio isSelected="false"/>
+        <Radio isSelected="false"/>
 
- 		    <Switch isSelected="false"/>
+        <Switch isSelected="false"/>
+
+        <Modal isShow={this.state.modalIsShow}/>
         </div> 
     );
   }
@@ -56,37 +77,4 @@ var Radio = require('../components/radio/index');
           <App/>,
         document.body
       );
-
-   /*
- var  App = React.createClass({
-  getInitialState:function() {
-    return { alertIsShow: false ,toastIsShow:false};
-  },
-  showAlert:function(){
-    this.setState({
-      alertIsShow: true
-    });
-  },
-  showToast:function() {
-   this.setState({
-      toastIsShow: true
-    });
-  }, 
-  render:function() {
-    return <div>
-<div onClick={this.showAlert}>Alert<div>
-<div  onClick={this.showToast}>Toast<div> 
-
-
-     <Alert isShow={this.state.alertIsShow} message="警告"  /><br/><br/>
-
-             <Toast isShow={this.state.toastIsShow} message="提示" duration="2000" /><br/><br/>
  
- 		    <Radio isSelected="false"/>
-
- 		    <Switch isSelected="false"/>
-    </div>;
-  }
-});
-
-React.render(<App />, document.body)*/
